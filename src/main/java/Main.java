17d7538995243986b51d6381823883862e5feec5
John Lee <ljonghan@gmail.com>
2022-07-25 12:39:16 -0400

    added getState()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import static java.lang.Thread.State.TERMINATED;
+
 class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> System.out.println("Hello from Thread 1"));
@@ -27,6 +31,12 @@ class Main {
     }
 
     public static void printNameOfTerminatedThread(Thread[] threads) {
-        
+        String msg = "";
+        for(Thread thread: threads){
+            msg = thread.getState() == TERMINATED
+                    ? "TERMINATED THREAD " + thread.getName()
+                    : "";
+        }
+        System.out.println(msg);
     }
 }
